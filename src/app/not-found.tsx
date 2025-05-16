c4b9b90ede9da0cd57ac150e48ffa7604404b4fc
lquinteroc2 <149559088+lquinteroc2@users.noreply.github.com>
2025-05-16 11:44:48 -0500

    errores vercel1

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 export default function NotFound() {
@@ -5,7 +6,13 @@ export default function NotFound() {
     <div className='flex flex-col justify-center items-center border-[black] border text-center h-screen w-[100%]'>
       <h1 className='text-7xl text-center font-bold text-blue-400'>404</h1>
       <h2 className='text-center'>Not Found</h2>
-      <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/404/404-computer.svg" alt="Image-not-Found"  width={300}/>
+      <Image
+      src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/404/404-computer.svg"
+      alt="Image-not-Found"
+      width={300}
+      height={300} // altura estimada; podés ajustarla o usar layout "responsive"
+      unoptimized // necesario si usás imágenes remotas y no están en next.config.js
+    />
       <p>Could not find requested resource</p>
       <Link href="/home" className='py-3'>Return <p className=' text-4xl font-bold text-blue-400 animate-pulse duration-1000 '>Home 🏠</p> </Link>
     </div>
